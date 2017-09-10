d79b2a5d8234dd475721cd92884a3f4e78e9a888
Tanuj Sharma <tanuj.goodboy77@gmail.com>
2017-09-10 18:06:06 +0530

    separates places data

---
+++ js/places.js
@@ -0,0 +1,70 @@
+var locationData = [{
+    locationName: 'Barista Cafe',
+    latLng: {
+      lat: 26.1860244,
+      lng: 78.2048169
+    },
+    address: "Neo Meridian Mall, Gwalior, Madhya Pradesh, India",
+    id: "56c872ce498e18ceae575ecc"
+  },
+
+  {
+    locationName: 'Gwalior Zoo',
+    latLng: {
+      lat: 26.2103311,
+      lng: 78.169084
+    },
+    address: "Italian Garden Rd, Lashkar, Gwalior, Madhya Pradesh, India",
+    id: "51d93197498e03f38353b9e0"
+  },
+
+  {
+    locationName: 'Jaivilas Palace',
+    latLng: {
+      lat: 26.200663,
+      lng: 78.1667488
+    },
+    address: "Lashkar, Gwalior, Madhya Pradesh, India",
+    id: "506ab860e4b012a83da5def9"
+  },
+
+  {
+    locationName: 'Sun Temple',
+    latLng: {
+      lat: 26.2364084,
+      lng: 78.2123883
+    },
+    address: "Morar, Gwalior, Madhya Pradesh, India",
+    id: "4f923bf1e4b01eb7515f8ddd"
+  },
+
+  {
+    locationName: 'Dindayal City Mall',
+    latLng: {
+      lat: 26.2093589,
+      lng: 78.1641293
+    },
+    address: "Shinde Ki Chhawani, Gwalior, Madhya Pradesh, India",
+    id: "4f34cf64e4b0debe1e717d99"
+  },
+
+  {
+    locationName: 'Gurudwara Shri Data Bandi Chhor',
+    latLng: {
+      lat: 26.2204196,
+      lng: 78.1646832
+    },
+    address: "Gwalior Fort, Gwalior, Madhya Pradesh, India",
+    id: "4f7d9182e4b024f467d2c6f1"
+  },
+
+  {
+    locationName: 'Sahastra Bahu Ka Mandir',
+    latLng: {
+      lat: 26.2284753,
+      lng: 78.165914
+    },
+    address: "Fort Campus, Near Post Office, Gwalior Fort, Gwalior, Madhya Pradesh, India",
+    id: "56e03057cd10691b23f64058"
+  }
+];
